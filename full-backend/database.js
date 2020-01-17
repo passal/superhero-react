f06461bdb672397fd176b8bf35b03847c237209a
michal <michaligld@gmail.com>
2020-01-17 21:27:06 +0200

    replaced file locations to make front and back run together

--- full-backend/database.js
+++ full-backend/database.js
@@ -3,13 +3,14 @@ const mysql = require("mysql");
 const sqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Th,hmur1",
+    password: "23542354",
     database: "projectcs"
 });
 
 sqlConnection.connect( (err) => {
     if(err){
         console.log("Connection Failed");
+        console.log(err);
     } else {
         console.log("Connected");
     }
