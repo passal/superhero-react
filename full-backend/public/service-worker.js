222afd60116831548fa9a0c5c4d2622f81c63506
michal <michaligld@gmail.com>
2020-01-24 15:21:21 +0200

    cartResult works

--- full-backend/public/service-worker.js
+++ full-backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b818859ed3e9e4e4812b6edd897b54e.js"
+  "/precache-manifest.59fe8381afaf946b91fe2be87c819eab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
